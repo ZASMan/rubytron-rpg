f9291c6ffeeec9fd3eb7411bdc003d3af243afd1
zasman <zanesandin@gmail.com>
2022-06-21 12:50:13 -0400

    Fixes some loading order stuff.

--- rubytron_rpg.rb
+++ rubytron_rpg.rb
@@ -1,8 +1,10 @@
 require 'sinatra'
-
+# Base Classes
+require_relative 'models/base_action.rb'
+require_relative 'models/base_entity.rb'
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/models/*.rb'].each {|file| require file }
-Dir[File.dirname(__FILE__) + '/controllers/*.rb'].each {|file| require file }
+# Dir[File.dirname(__FILE__) + '/controllers/*.rb'].each {|file| require file }
 
 
 get '/' do
